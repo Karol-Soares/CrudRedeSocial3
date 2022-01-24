c2d3b3646f311c62a4dc1dc75bdb11a1314867e7
Karol  Soares <karolina.soares@sumup.com>
2022-01-24 00:17:59 -0300

    Atividade crud 3

---
+++ RedeSocial/src/test/java/br/org/generation/RedeSocial/RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.RedeSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
